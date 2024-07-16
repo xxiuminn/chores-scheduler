448dfbaad9961acf6a0b611c11959b751496ba48
xxiuminn <xiumin1997@gmail.com>
2024-07-17 00:17:03 +0800

    Update Calendar.jsx

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -3,17 +3,24 @@ import styles from "./Calendar.module.css";
 import CalendarRow from "./CalendarRow";
 
 const Calendar = () => {
-  const [date, setDate] = useState(new Date());
-  const [year, setYear] = useState(date.getFullYear());
-  const [monthIndex, setMonthIndex] = useState(date.getMonth());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [year, setYear] = useState(selectedDate.getFullYear());
+  const [monthIndex, setMonthIndex] = useState(selectedDate.getMonth());
+  const yearStart = () => {
+    const firstDateOfYear = new Date(year, 0, 1);
+    let dayOfFirstDateOfYear = firstDateOfYear.getDay();
+    const dateOfFirstWeekOfYear = new Date(year, 0, 1 - dayOfFirstDateOfYear);
+    return dateOfFirstWeekOfYear;
+  };
   const [week, setWeek] = useState(
-    Math.ceil((date - new Date(year, 0, 1) + 1) / 86400000 / 7)
+    Math.ceil((selectedDate - yearStart() + 1) / 86400000 / 7)
   );
-  let thisMonth = date.toLocaleString("en-GB", {
+  let thisMonth = selectedDate.toLocaleString("en-GB", {
     month: "long",
     year: "numeric",
   });
 
+  console.log(yearStart());
   const monthsInYear = [
     "Jan",
     "Feb",
@@ -30,26 +37,54 @@ const Calendar = () => {
   ];
   const daysInWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  // const firstDayOfMonth = new Date(year, monthIndex, 1);
-  // const firstDayOfMonthIndex = firstDayOfMonth.getDay();
-  //underflows
-  const lastDayOfMonth = new Date(year, monthIndex + 1, 0);
-  const daysInMonth = lastDayOfMonth.getDate();
+  const firstDateOfMonth = new Date(year, monthIndex, 1);
+  console.log("firstDateOfMonth", firstDateOfMonth);
+  console.log("firstDateOfMonth", firstDateOfMonth.getDate());
+  // const dayOfFirstDateOfMonth = firstDateOfMonth.getDay();
+  // console.log("dayOfFirstDateOfMonth", dayOfFirstDateOfMonth);
+  console.log(new Date(year, monthIndex, -1));
 
-  //basically trying to get all the dates of the 'selected' month & put them in an array.
-  let monthArr = [];
-  for (let date = 1; date < daysInMonth; date++) {
-    //assigning the dates to their respective days.
-    const day = new Date(year, monthIndex, date).getDay();
-    const weekOfDate = Math.ceil(
-      (new Date(year, monthIndex, date) - new Date(year, 0, 1) + 1) /
-        86400000 /
-        7
-    );
-    monthArr.push({ day, date, weekOfDate, monthIndex, year });
-  }
+  const firstWeekOfMonth = Math.ceil(
+    (new Date(year, monthIndex, 1) - yearStart() + 1) / 86400000 / 7
+  );
+  console.log("firstWeekOfMonth", firstWeekOfMonth);
 
-  // console.log(Math.ceil((date - new Date(year, 0, 1) + 1) / 86400000 / 7));
+  const lastDateOfMonth = new Date(year, monthIndex + 1, 0);
+  console.log("lastDateOfMonth", lastDateOfMonth);
+
+  const dayOfLastDateOfMonth = lastDateOfMonth.getDay();
+  const daysInMonth = lastDateOfMonth.getDate();
+  console.log("daysInMonth", daysInMonth);
+
+  const lastWeekOfMonth = Math.ceil(
+    (new Date(year, monthIndex, daysInMonth) - yearStart() + 1) / 86400000 / 7
+  );
+  console.log("lastWeekOfMonth", lastWeekOfMonth);
+
+  const getMonthArr = () => {
+    let monthArr = [];
+    let dayOfFirstDateOfMonth = firstDateOfMonth.getDay();
+    while (dayOfFirstDateOfMonth > 0) {
+      // monthArr.push(new Date(year, monthIndex, 1 - dayOfFirstDateOfMonth));
+      // dayOfFirstDateOfMonth -= 1;
+      const fullDate = new Date(year, monthIndex, 1 - dayOfFirstDateOfMonth);
+      const date = fullDate.getDate();
+      const weekOfDate = Math.ceil((fullDate - yearStart() + 1) / 86400000 / 7);
+      monthArr.push({ weekOfDate, date });
+      dayOfFirstDateOfMonth -= 1;
+    }
+    for (let date = 1; date < daysInMonth + 1; date++) {
+      const day = new Date(year, monthIndex, date).getDay();
+      const weekOfDate = Math.ceil(
+        (new Date(year, monthIndex, date) - yearStart() + 1) / 86400000 / 7
+      );
+      // monthArr.push(new Date(year, monthIndex, date));
+      monthArr.push({ weekOfDate, date });
+    }
+    return monthArr;
+  };
+
+  console.log(getMonthArr());
 
   return (
     <div className="container">
@@ -69,7 +104,7 @@ const Calendar = () => {
       </div>
       {/* calendar weekly view - only showing the 'selected' week */}
       <div className="row text-center">
-        {monthArr.map((item) => {
+        {getMonthArr().map((item) => {
           if (item.weekOfDate === week) {
             return <div className="col">{item.date}</div>;
           }
