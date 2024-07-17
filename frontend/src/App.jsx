7eef4bb21f3cf4ff4ed4a153ddfb39045953a347
xxiuminn <xiumin1997@gmail.com>
2024-07-17 21:27:51 +0800

    add tanstack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Calendar from "./components/Calendar";
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
+  const queryClient = new QueryClient();
 
   const handleShowLogin = () => {
     setShowLogin(!showLogin);
@@ -12,9 +14,11 @@ function App() {
 
   return (
     <>
-      <Calendar />
-      {/* {showLogin && <Login handleShowLogin={handleShowLogin} />}
+      <QueryClientProvider client={queryClient}>
+        <Calendar />
+        {/* {showLogin && <Login handleShowLogin={handleShowLogin} />}
       {!showLogin && <Register handleShowLogin={handleShowLogin} />} */}
+      </QueryClientProvider>
     </>
   );
 }
