e1a9900185c9eff4d0dc5bf8da0d35db66fdc1f6
xxiuminn <xiumin1997@gmail.com>
2024-07-17 21:28:21 +0800

    Update Calendar.jsx

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Calendar.module.css";
 import CalendarRow from "./CalendarRow";
 
@@ -6,81 +6,58 @@ const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [year, setYear] = useState(selectedDate.getFullYear());
   const [monthIndex, setMonthIndex] = useState(selectedDate.getMonth());
+
+  const daysInWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   const yearStart = () => {
     const firstDateOfYear = new Date(year, 0, 1);
     let dayOfFirstDateOfYear = firstDateOfYear.getDay();
     const dateOfFirstWeekOfYear = new Date(year, 0, 1 - dayOfFirstDateOfYear);
     return dateOfFirstWeekOfYear;
   };
+
   const [week, setWeek] = useState(
     Math.ceil((selectedDate - yearStart() + 1) / 86400000 / 7)
   );
+
   let thisMonth = selectedDate.toLocaleString("en-GB", {
     month: "long",
     year: "numeric",
   });
 
-  console.log(yearStart());
-  const monthsInYear = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const daysInWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  const firstDateOfMonth = new Date(year, monthIndex, 1);
-  console.log("firstDateOfMonth", firstDateOfMonth);
-  console.log("firstDateOfMonth", firstDateOfMonth.getDate());
-  // const dayOfFirstDateOfMonth = firstDateOfMonth.getDay();
-  // console.log("dayOfFirstDateOfMonth", dayOfFirstDateOfMonth);
-  console.log("testing", new Date(year, monthIndex, 32));
-
-  const firstWeekOfMonth = Math.ceil(
-    (new Date(year, monthIndex, 1) - yearStart() + 1) / 86400000 / 7
-  );
-  console.log("firstWeekOfMonth", firstWeekOfMonth);
-
-  const lastDateOfMonth = new Date(year, monthIndex + 1, 0);
-  console.log("lastDateOfMonth", lastDateOfMonth);
-
-  const daysInMonth = lastDateOfMonth.getDate();
-  console.log("daysInMonth", daysInMonth);
-
-  const lastWeekOfMonth = Math.ceil(
-    (new Date(year, monthIndex, daysInMonth) - yearStart() + 1) / 86400000 / 7
-  );
-  console.log("lastWeekOfMonth", lastWeekOfMonth);
-
   const getMonthArr = () => {
     let monthArr = [];
+    const firstDateOfMonth = new Date(year, monthIndex, 1);
+    // const firstWeekOfMonth = Math.ceil(
+    //   (new Date(year, monthIndex, 1) - yearStart() + 1) / 86400000 / 7
+    // );
+    const lastDateOfMonth = new Date(year, monthIndex + 1, 0);
+    const daysInMonth = lastDateOfMonth.getDate();
+    // const lastWeekOfMonth = Math.ceil(
+    //   (new Date(year, monthIndex, daysInMonth) - yearStart() + 1) / 86400000 / 7
+    // );
     let dayOfFirstDateOfMonth = firstDateOfMonth.getDay();
     let dayOfLastDateOfMonth = lastDateOfMonth.getDay();
+
+    //fill in dates for the first week of the month
     while (dayOfFirstDateOfMonth > 0) {
-      // monthArr.push(new Date(year, monthIndex, 1 - dayOfFirstDateOfMonth));
-      // dayOfFirstDateOfMonth -= 1;
       const fullDate = new Date(year, monthIndex, 1 - dayOfFirstDateOfMonth);
       const date = fullDate.getDate();
       const weekOfDate = Math.ceil((fullDate - yearStart() + 1) / 86400000 / 7);
       monthArr.push({ weekOfDate, date });
       dayOfFirstDateOfMonth -= 1;
     }
+
+    //fill in dates for the rest of the month
     for (let date = 1; date < daysInMonth + 1; date++) {
       const day = new Date(year, monthIndex, date).getDay();
       const weekOfDate = Math.ceil(
         (new Date(year, monthIndex, date) - yearStart() + 1) / 86400000 / 7
       );
-      // monthArr.push(new Date(year, monthIndex, date));
       monthArr.push({ weekOfDate, date });
     }
+
+    //fill in dates for the last week of the month
     for (let addDays = 1; addDays < 6 - dayOfLastDateOfMonth + 1; addDays++) {
       const fullDate = new Date(year, monthIndex, daysInMonth + addDays);
       const date = fullDate.getDate();
@@ -90,16 +67,28 @@ const Calendar = () => {
     return monthArr;
   };
 
-  console.log(getMonthArr());
+  useEffect(() => {
+    setYear(selectedDate.getFullYear());
+    setMonthIndex(selectedDate.getMonth());
+    setWeek(Math.ceil((selectedDate - yearStart() + 1) / 86400000 / 7));
+  }, [selectedDate]);
+
+  const handlePrev = () => {
+    setSelectedDate(new Date(selectedDate.getTime() - 7 * 24 * 60 * 60 * 1000));
+  };
+
+  const handleNext = () => {
+    setSelectedDate(new Date(selectedDate.getTime() + 7 * 24 * 60 * 60 * 1000));
+  };
 
   return (
     <div className="container">
       <div className="row text-center">
-        <div onClick={() => setWeek(week - 1)}>
+        <div onClick={handlePrev}>
           <i className="bi bi-arrow-left-circle"></i>
         </div>
         <div className="col">{thisMonth}</div>
-        <div onClick={() => setWeek(week + 1)}>
+        <div onClick={handleNext}>
           <i className="bi bi-arrow-right-circle"></i>
         </div>
       </div>
