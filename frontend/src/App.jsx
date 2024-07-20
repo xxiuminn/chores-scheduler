86ba70f66cab49455cb62b419dfb4b7021b02327
xxiuminn <xiumin1997@gmail.com>
2024-07-21 01:04:15 +0800

    add js-cookie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Cookies from "js-cookie";
 import UserContext from "./context/user";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,13 +16,21 @@ function App() {
     setShowLogin(!showLogin);
   };
 
+  useEffect(() => {
+    setAccessToken(Cookies.get("token"));
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <UserContext.Provider value={{ accessToken, setAccessToken }}>
-          {/* <Calendar /> */}
-          {showLogin && <Login handleShowLogin={handleShowLogin} />}
-          {!showLogin && <Register handleShowLogin={handleShowLogin} />}
+          {accessToken && <Calendar />}
+          {!accessToken && (
+            <>
+              {showLogin && <Login handleShowLogin={handleShowLogin} />}
+              {!showLogin && <Register handleShowLogin={handleShowLogin} />}
+            </>
+          )}
         </UserContext.Provider>
       </QueryClientProvider>
     </>
