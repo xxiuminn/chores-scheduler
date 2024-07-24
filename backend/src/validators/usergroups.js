c068f6db19ec8b949c6668831342568608cbbb33
xxiuminn <xiumin1997@gmail.com>
2024-07-24 13:19:20 +0800

    add validators for user groups

---
+++ backend/src/validators/usergroups.js
@@ -0,0 +1,21 @@
+const { body } = require("express-validator");
+
+const validateCreateUserGroup = [
+  body("usergroup_name", "usergroup name required").notEmpty().isString(),
+  body("account_type", "account type required").notEmpty().isString(),
+  body("uuid", "uuid is required").notEmpty().isString(),
+];
+
+const validateUpdateAccountType = [
+  body("usergroup_id", "user group id is required").notEmpty().isInt(),
+  body("account_type", "account type required").notEmpty().isString(),
+];
+
+const validateGetGroupMembers = [
+  body("usergroup_id", "user group id required").notEmpty().isInt(),
+];
+module.exports = {
+  validateCreateUserGroup,
+  validateUpdateAccountType,
+  validateGetGroupMembers,
+};
