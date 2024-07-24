6ee5bf5267098fc2f6513dae7624cb64d778a33f
xxiuminn <xiumin1997@gmail.com>
2024-07-25 00:28:03 +0800

    add some console.log for debugging

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const useFetch = () => {
+  console.log("mutation start");
   const fetchData = async (endpoint, method, body, token) => {
     const res = await fetch(import.meta.env.VITE_SERVER + endpoint, {
       method,
@@ -15,6 +16,7 @@ const useFetch = () => {
     }
 
     const data = await res.json();
+    console.log("mutation end");
     return data;
   };
 
