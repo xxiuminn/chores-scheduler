b7de0b72263eb0df0d20b317ba24742b9babf511
xxiuminn <xiumin1997@gmail.com>
2024-07-24 13:18:12 +0800

    add validators on tasks routers

--- backend/src/routers/tasks.js
+++ backend/src/routers/tasks.js
@@ -13,15 +13,44 @@ const {
 } = require("../controllers/tasks");
 const router = express.Router();
 
-router.put("/create", createTaskGroup);
-router.post("/usergroup", getTasksByUserGroup);
-router.post("/user", getTasksByUser);
-router.post("/task", getTask);
-router.delete("/deleteone", delTask);
-router.delete("/deleteall", delAllTasks);
-router.delete("/deletefollowing", delFollowingTasks);
-router.post("/updateone", updateTask);
-router.post("/updateall", updateAllTasks);
-router.post("/updatefollowing", updateFollowingTasks);
+const {
+  validateCreateTaskGroup,
+  validateGetTaskByUserGroup,
+  validateGetTaskByUser,
+  validateGetTask,
+  validateDelTask,
+  validateDelAllTasks,
+  validateDelFollowingTasks,
+  validateUpdateTask,
+} = require("../validators/tasks");
+
+const checkErrors = require("../validators/checkErrors");
+const { check } = require("express-validator");
+
+router.put("/create", validateCreateTaskGroup, checkErrors, createTaskGroup);
+router.post(
+  "/usergroup",
+  validateGetTaskByUserGroup,
+  checkErrors,
+  getTasksByUserGroup
+);
+router.post("/user", validateGetTaskByUser, checkErrors, getTasksByUser);
+router.post("/task", validateGetTask, checkErrors, getTask);
+router.delete("/deleteone", validateDelTask, checkErrors, delTask);
+router.delete("/deleteall", validateDelAllTasks, checkErrors, delAllTasks);
+router.delete(
+  "/deletefollowing",
+  validateDelFollowingTasks,
+  checkErrors,
+  delFollowingTasks
+);
+router.post("/updateone", validateUpdateTask, checkErrors, updateTask);
+router.post("/updateall", validateUpdateTask, checkErrors, updateAllTasks);
+router.post(
+  "/updatefollowing",
+  validateUpdateTask,
+  checkErrors,
+  updateFollowingTasks
+);
 
 module.exports = router;
