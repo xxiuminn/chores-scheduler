fe362d7e9f67e19d2de5035a6ba9b09e8a44ed5f
xxiuminn <xiumin1997@gmail.com>
2024-07-25 00:26:46 +0800

    task modal able to create tasks & task group

--- frontend/src/components/AddTaskModal.jsx
+++ frontend/src/components/AddTaskModal.jsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
+import { jwtDecode } from "jwt-decode";
 
 const AddTaskModal = (props) => {
+  const useCtx = useContext(UserContext);
+  const queryClient = useQueryClient();
   const [deadline, setDeadline] = useState("");
   const [title, setTitle] = useState("");
   const [isRecurring, setIsRecurring] = useState("");
@@ -10,6 +16,12 @@ const AddTaskModal = (props) => {
   const [rule, setRule] = useState("");
   const [createdBy, setCreatedBy] = useState("");
   const [schedule, setSchedule] = useState("");
+  const fetchData = useFetch();
+  const [members, setMembers] = useState([]);
+
+  const accessToken = useCtx.accessToken;
+  const claims = jwtDecode(accessToken);
+  // console.log(claims);
 
   useEffect(() => {
     if (props.modalDate) {
@@ -27,10 +39,74 @@ const AddTaskModal = (props) => {
     setSchedule(value);
     if (value === "ONCE") {
       setIsRecurring(0);
+      setRule("");
+      setIsRotate("");
     } else {
       setIsRecurring(1);
       setRule(value);
     }
+  };
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["members"],
+    queryFn: async () => {
+      return await fetchData(
+        "/usergroups/members",
+        "POST",
+        {
+          usergroup_id: claims.group_id,
+        },
+        accessToken
+      );
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      setMembers(data);
+    }
+    // console.log(data);
+  }, [data]);
+
+  console.log(
+    isRecurring,
+    title,
+    deadline,
+    assignedUser,
+    claims.uuid,
+    isRotate,
+    rule,
+    claims.group_id,
+    accessToken
+  );
+
+  const { mutate } = useMutation({
+    mutationFn: async () =>
+      await fetchData(
+        "/tasks/create",
+        "PUT",
+        {
+          is_recurring: isRecurring,
+          title,
+          deadline,
+          assigned_user: assignedUser,
+          created_by: claims.uuid,
+          is_rotate: isRotate,
+          rule,
+          usergroup_id: claims.group_id,
+        },
+        accessToken
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["tasks"]);
+      console.log("successful");
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutate();
+    console.log("form submitted");
   };
 
   return (
@@ -38,7 +114,7 @@ const AddTaskModal = (props) => {
       <div
         className="modal fade"
         id="addtaskmodal"
-        tabIndex="-1"
+        tabindex="-1"
         aria-labelledby="addtaskmodal"
         aria-hidden="true"
       >
@@ -56,7 +132,8 @@ const AddTaskModal = (props) => {
 
             <form
               className="d-flex justify-content-center align-items-center needs-validation"
-              noValidate
+              novalidate
+              onSubmit={handleSubmit}
             >
               <div className="d-flex flex-column justify-content-center align-items-start">
                 <div className="mt-2">
@@ -158,6 +235,9 @@ const AddTaskModal = (props) => {
                           name="rotate"
                           id="no"
                           value={0}
+                          onChange={(e) =>
+                            setIsRotate(parseInt(e.target.value))
+                          }
                           required
                         />
                         <label className="form-check-label" htmlFor="no">
@@ -172,6 +252,9 @@ const AddTaskModal = (props) => {
                           name="rotate"
                           id="yes"
                           value={1}
+                          onChange={(e) =>
+                            setIsRotate(parseInt(e.target.value))
+                          }
                           required
                         />
                         <label className="form-check-label" htmlFor="yes">
@@ -189,12 +272,17 @@ const AddTaskModal = (props) => {
                 <select
                   className="form-select"
                   aria-label=".form-select members"
+                  required
                   onChange={(e) => setAssignedUser(e.target.value)}
+                  value={assignedUser}
                 >
-                  <option selected>Pick a member</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                  <option value="" disabled selected>
+                    Pick a member
+                  </option>
+                  {isSuccess &&
+                    data.map((member) => {
+                      return <option value={member.uuid}>{member.name}</option>;
+                    })}
                 </select>
 
                 <button className="mt-4 mb-5 align-self-center" type="submit">
