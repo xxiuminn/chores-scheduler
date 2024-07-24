bfdd8fbc60942515ca4809235f48eb9765bf581c
xxiuminn <xiumin1997@gmail.com>
2024-07-25 00:27:46 +0800

    create join user group modal

---
+++ frontend/src/components/JoinUserGroupModal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const JoinUserGroupModal = () => {
+  return (
+    <div
+      className="modal fade"
+      id="addtaskmodal"
+      tabIndex="-1"
+      aria-labelledby="addtaskmodal"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content"></div>
+      </div>
+    </div>
+  );
+};
+
+export default JoinUserGroupModal;
