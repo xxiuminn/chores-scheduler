9cc4120a15cea185c8b75234cfc85559f053f821
xxiuminn <xiumin1997@gmail.com>
2024-07-24 13:18:32 +0800

    add validators for user groups routers

--- backend/src/routers/usergroups.js
+++ backend/src/routers/usergroups.js
@@ -6,10 +6,21 @@ const {
   updateAccountType,
   getGroupMembers,
 } = require("../controllers/usergroups");
+const {
+  validateCreateUserGroup,
+  validateUpdateAccountType,
+  validateGetGroupMembers,
+} = require("../validators/usergroups");
+const checkErrors = require("../validators/checkErrors");
 
 router.post("/seed/accounts", seedAccountTypes);
-router.put("/usergroup", createUserGroup);
-router.post("/accounttype", updateAccountType);
-router.post("/members", getGroupMembers);
+router.put("/usergroup", validateCreateUserGroup, checkErrors, createUserGroup);
+router.post(
+  "/accounttype",
+  validateUpdateAccountType,
+  checkErrors,
+  updateAccountType
+);
+router.post("/members", validateGetGroupMembers, checkErrors, getGroupMembers);
 
 module.exports = router;
