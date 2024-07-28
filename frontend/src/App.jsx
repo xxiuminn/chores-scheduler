35b172c037155d19a34e8176a05d6ce7005654d7
xxiuminn <xiumin1997@gmail.com>
2024-07-28 14:28:59 +0800

    remove useContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,28 +7,32 @@ import Register from "./pages/Register";
 import UserProfile from "./pages/UserProfile";
 import Members from "./pages/Members";
 import Calendar from "./components/Calendar";
+import JoinGroup from "./pages/JoinGroup";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [accessToken, setAccessToken] = useState(localStorage.getItem("token"));
+  // const [accessToken, setAccessToken] = useState(localStorage.getItem("token"));
 
   // console.log(localStorage.getItem("token"));
+
+  // fetch user info
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <UserContext.Provider value={{ accessToken, setAccessToken }}>
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/login" />} />
-            <Route path="/board" element={<Calendar />} />
-            {/* <Route path="/board" element={<Calendar members={membersData} />} /> */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/userprofile" element={<UserProfile />} />
-            <Route path="/members" element={<Members />} />
-          </Routes>
-        </UserContext.Provider>
+        {/* <UserContext.Provider value={{}}> */}
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/board" element={<Calendar />} />
+          {/* <Route path="/board" element={<Calendar members={membersData} />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/joingroup" element={<JoinGroup />} />
+        </Routes>
+        {/* </UserContext.Provider> */}
       </QueryClientProvider>
     </>
   );
