37ef4abb4eee66ff5e802fe5506aa57f00cb7292
xxiuminn <xiumin1997@gmail.com>
2024-07-28 14:30:36 +0800

    create authFree & authPaid middleware (WIP)

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const db = require("../db/db");
 
 const auth = (req, res, next) => {
   if (!("authorization" in req.headers)) {
@@ -22,4 +23,65 @@ const auth = (req, res, next) => {
   }
 };
 
-module.exports = { auth };
+const authFree = (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+      console.log(decoded);
+      if (decoded.group_id && decoded.membership === "ACTIVE") {
+        console.log("yes");
+        req.decoded = decoded;
+        console.log(decoded.group_id, decoded.membership);
+        console.log(token);
+        next();
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.error(error.message);
+      return res.status(403).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+const authPaid = async (req, res, next) => {
+  if (!("authorization" in req.headers)) {
+    return res.status(400).json({ status: "error", msg: "token required" });
+  }
+
+  const token = req.headers["authorization"].replace("Bearer ", "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+      const accountTypeArr = await db.query(
+        "SELECT account_type FROM user_groups WHERE id=$1",
+        [decoded.group_id]
+      );
+      const accountType = accountTypeArr.rows[0].account_type;
+      if (
+        decoded.group_id &&
+        decoded.membership === "ACTIVE" &&
+        accountType === "PAID"
+      ) {
+        req.decoded = decoded;
+        next();
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.error(error.message);
+      return res.status(403).json({ status: "error", msg: "not authorised" });
+    }
+  } else {
+    return res.status(403).json({ status: "error", msg: "forbidden" });
+  }
+};
+
+module.exports = { auth, authFree, authPaid };
