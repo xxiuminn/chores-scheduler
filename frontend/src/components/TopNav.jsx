1c78a37bca4fefcf4ffaa005f5e4b847c2597c0a
xxiuminn <xiumin1997@gmail.com>
2024-07-30 18:28:11 +0800

    update top nav

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./Calendar.module.css";
+import { jwtDecode } from "jwt-decode";
+import NavBar from "./NavBar";
+import { useQueryClient } from "@tanstack/react-query";
 
 const TopNav = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const logout = () => {
-    // useCtx.setAccessToken("");
     console.log("clearing token from local storage");
+    queryClient.removeQueries();
     localStorage.removeItem("token");
-    // console.log("clearing token from context");
-    // useCtx.setAccessToken("");
     console.log("navigating to login page");
     navigate("/login");
     console.log("logged out");
   };
 
   return (
-    <>
-      <div className={styles.toprightnav}>
+    <div className="row mx-4 my-3">
+      <div className={styles.topnav}>
+        <NavBar />
+        <div>
+          Welcome <b>{jwtDecode(localStorage.getItem("token")).name}</b>
+        </div>
         <div className="dropdown">
           <button
             className={`dropdown-toggle ${styles.profile}`}
@@ -40,7 +46,7 @@ const TopNav = () => {
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
